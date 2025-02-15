494c5d22aba174e11af7191580bbb9999f51ff68
Mauricio Valencia <>
2025-02-15 13:30:50 -0300

    chore: setting home interface ui or App ui

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../shared/components/Layout/Navbar.tsx";
+import Footer from "../shared/components/Layout/Footer.tsx";
+import "../styles/App.css";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <div className="content">
+
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+// <reference models="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_USERS_URL: string;
+    readonly VITE_ENV: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
